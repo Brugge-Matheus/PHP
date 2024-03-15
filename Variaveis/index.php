c51fb8c178bbe8da1086ae813dcc1312a92221b2
Matheus Brugge Milczwski <128723144+Brugge-Matheus@users.noreply.github.com>
2024-03-14 23:48:41 -0300

    studying php

---
+++ Variaveis/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        $name = "James";
+        $lastName = "Jhones";
+        $curso = "PHP";
+        $ano = date("y");
+
+        echo "My name is $name $lastName";
+        echo <<< TESTE
+            Iam studying $curso
+            in $ano!
+        TESTE;
+    ?>
+    
+</body>
+</html>
