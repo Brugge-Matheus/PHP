b927b15216a3e1aac6fe31b9da8f26c1c6e7ff2b
Brugge-Matheus <128723144+Brugge-Matheus@users.noreply.github.com>
2024-04-07 23:27:24 -0300

    new changes

---
+++ PHP/Estruturas_de_controle/foreach.php
@@ -0,0 +1,42 @@
+<?php 
+    $ing = array('farinha', 'ovo', 'leite', 'fermento');
+
+    foreach($ing as $value) {
+        echo "$value<br>";
+    }
+
+    echo "<hr>";
+    print_r($ing);
+    echo "<hr>";
+
+    foreach($ing as $key => $value) {
+        echo "$value, $key <br>";
+    }
+
+    $line = '-';
+
+    for($column = 0; $column <= 20; $column++) {
+        for($row = 0; $row <= $column; $row++) {
+            echo "$line";
+        }
+        echo "<br>";
+    }
+    echo "<hr>";
+
+    for($column = 0; $column <= 10; $column++) {
+        for($row = 0; $row <= 10; $row++) {
+            echo $line;
+        }
+        echo "<br>";
+    }
+
+        
+    $numbers = array(10, 20, 30, 40, 50, 60);
+
+    foreach($numbers as $key => $value) {
+        echo "Resultado" . ($key + 1) . ":\n" . ($value + $value) . "<br>";
+        echo "<br>";
+    }
+    
+
+?>
