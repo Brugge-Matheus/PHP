80b65f43e08b4cf7e24216325127edc9b1c27466
Matheus Brugge Milczwski <128723144+Brugge-Matheus@users.noreply.github.com>
2024-04-26 19:41:52 -0300

    CHANGES

--- PHP/Banco_de_dados/pdo.php
+++ PHP/Banco_de_dados/pdo.php
@@ -6,8 +6,8 @@ $passwd = '';
 
 $pdo = new PDO("mysql:dbname=$database; host=$host", "$user", "");
 
-// $sql = $pdo->query("INSERT INTO usuarios VALUES (DEFAULT, 'Matheus Brugge', '2005-10-29')");
-$sql = $pdo->query("SELECT * FROM usuarios");
+$sql = $pdo->query("INSERT INTO usuarios VALUES (DEFAULT, 'Matheus Brugge', '2005-10-29')");
+// $sql = $pdo->query("SELECT * FROM usuarios");
 $dados = $sql->fetchAll(PDO::FETCH_ASSOC);
 
 echo "<pre>";
