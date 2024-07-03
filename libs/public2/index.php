f162af2b84adadbb76408e4afc10184472b2b7e5
Brugge-Matheus <128723144+Brugge-Matheus@users.noreply.github.com>
2024-07-02 23:52:51 -0300

    changes

--- libs/public2/index.php
+++ libs/public2/index.php
@@ -1,32 +1,56 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trabalhando com Radios</title>
+    <style>
+        * {
+            font-family: sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <h2>Escolha as opção que deseja</h2>
+    <form action="<?php $_SERVER['PHP_SELF']?>" method="post">
+            <input type="checkbox" name="foods[]" value="Maça">Maça
+            <br>
+            <input type="checkbox" name="foods[]" value="Abacate">Abacate
+            <br>
+            <input type="checkbox" name="foods[]" value="Banana">Banana
+            <br>
+            <input type="checkbox" name="foods[]" value="Kiwi">Kiwi
+            <br>
+            <input type="checkbox" name="foods[]" value="Tomate">Tomate
+            <br>
 
-    function routes() {
-        return [
-            'POST' => [
-                '/' => 'Home@action',
-                '/login' => 'Login@action'
-            ],
+        <input type="submit" name="submit-credit-card" value="Enviar">
+    </form>
 
-            'GET' => [
-                '/' => 'Home@index',
-                '/login' => 'Login@index'
-            ]   
-        ];
-    }
+    <div class="response">
+        <pre>
+            <?php
+            $foods = null;
+            if(isset($_POST['submit-credit-card'])) {
 
-    function methodPost() {
-        return 'POST';
-    }
+                if(isset($_POST['foods'])) {
+                    $foods = $_POST['foods'];
+                    $foods = array_combine($foods, array_map(function($field) {
+                        return $field;
+                    }, $foods));
 
-    function methodGet() {
-        return 'GET';
-    }
+                    $foods = implode(', ', $foods);
 
-    $route = routes();
-    $POST = methodPost();
-    $GET = methodGet();
+                    echo "<strong>Você escolheu:</strong> " . $foods .".";
 
-    echo "<pre>";
-    var_dump($route[$GET]);
-    echo "</pre>";
-?>
+                } else {
+                    echo "<strong>Escolha uma opção válida!</strong>";
+                    
+                }
+
+            }    
+            ?>
+        </pre>
+    </div>
+</body>
+</html>
